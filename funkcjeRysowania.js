e1f6eb12965029908f71d1b77cceffeb6a3aebf2
GalaktyczneBiznesy <157284792+GalaktyczneBiznesy@users.noreply.github.com>
2024-03-17 11:10:55 +0100

    Update funkcjeRysowania.js

--- funkcjeRysowania.js
+++ funkcjeRysowania.js
@@ -10,10 +10,10 @@ function rysuj(plutno,budowaObrazka)
 		ctx.lineWidth=element.grubosc||0
 		ctx.strokeStyle=element.kolorLini||`rgba(0,0,0,0)`
 		ctx.fillStyle=element.kolor||`rgba(0,0,0,0)`
+		ctx.filter=`blur(${element.rozmycie||0}px)`
 		ctx.beginPath()
 		if (Array.isArray(element.srodek) && element.promien>0)
 		{
-			if (!Array.isArray(element.srodek) || !Number(element.promien)){return}
 			if (Array.isArray(element.zakresKatow))
 			{
 				ctx.arc(element.srodek[1], element.srodek[0], element.promien, element.zakresKatow[0]*Math.PI, element.zakresKatow[1]*Math.PI);
@@ -23,15 +23,18 @@ function rysuj(plutno,budowaObrazka)
 				ctx.arc(element.srodek[1], element.srodek[0], element.promien, 0, 2*Math.PI);
 			}
 		}
-		else
+		else if ((element.punkty||[]).length>2)
 		{
-			if ((element.punkty||[]).length<3){return}
 			ctx.moveTo(element.punkty[0][1],element.punkty[0][0])
 			for (let a=1 ; a<element.punkty.length ; a++)
 			{
 				ctx.lineTo(element.punkty[a][1],element.punkty[a][0])
 			}
 			ctx.lineTo(element.punkty[0][1],element.punkty[0][0])
+		}
+		else
+		{
+			console.log("nieznany kształt: ",element)
 		}
 		if (element.kolor)
 		{
@@ -43,5 +46,4 @@ function rysuj(plutno,budowaObrazka)
 		}
 		ctx.closePath()
 	}
-	ctx.filter=`blur(${50}px)`
 }
